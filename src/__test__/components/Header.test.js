239b6e1e9c0fb9f7b4fa2d07d34477210d5f872e
Andres Reyes <ar97yf@gmail.com>
2020-12-21 18:14:35 -0500

    [ADD] jest

---
+++ src/__test__/components/Header.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import { create } from 'react-test-renderer';
+import ProviderMock from '../../__mocks__/ProviderMock';
+import Header from '../../components/Header';
+import ProductMock from '../../__mocks__/ProductMock';
+
+describe('<Header />', () => {
+  test('Render del componente Header ', () => {
+    const header = shallow(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>
+    );
+    expect(header.length).toEqual(1);
+  });
+  test('Render del titulo', () => {
+    const header = mount(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>
+    );
+    expect(header.find('.Header-title').text()).toEqual('Platzi Store');
+  });
+});
+
+describe('Header snapshot', () => {
+  test('Comprobar la UI de Header', () => {
+    const header = create(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>
+    );
+    expect(header.toJSON()).toMatchSnapshot();
+  });
+});
